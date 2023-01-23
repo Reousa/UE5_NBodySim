a576569121c82bb2cfe05499beac1d8b784dc65d
Karim Rizk <karim@rizk.dev>
2023-01-23 02:23:01 +0200

    Add atomic lock

---
+++ Source/NBodySim/Public/Core/Threading/FAtomicScopeLock.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "FAtomicMutex.h"
+
+/**
+ * @brief Scoped lock that uses FAtomicMutex
+ */
+class FAtomicScopeLock
+{
+private:
+	FAtomicMutex& Mutex;
+	
+public:
+	FORCEINLINE explicit FAtomicScopeLock(FAtomicMutex& InMutex) noexcept :
+	Mutex(InMutex)
+	{
+		Mutex.SpinWaitLock();
+	}
+
+	FORCEINLINE ~FAtomicScopeLock() noexcept
+	{
+		Mutex.Unlock();
+	}
+};
